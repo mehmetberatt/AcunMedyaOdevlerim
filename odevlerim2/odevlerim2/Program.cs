bae63a850bcf7198dd38523206caf700d5e2d069
mehmetberatt <mehmetberattrn@gmail.com>
2025-03-15 03:05:00 +0300

    namespaceodevi

---
+++ odevlerim2/odevlerim2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MathOperations; // Namespace'i içe aktardık
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Hesaplama hesap = new Hesaplama();
+
+            int toplam = hesap.Topla(10, 5);
+            int carpim = hesap.Carp(10, 5);
+
+            Console.WriteLine($"Toplama Sonucu: {toplam}");
+            Console.WriteLine($"Çarpma Sonucu: {carpim}");
+        }
+    }
+}
---
+++ odevlerim2/odevlerim2/hesaplama.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathOperations
+{
+    public class Hesaplama
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
